3d7e2f2192b8b946b6c5fcd91daf9376f9f15ab1
Caique Moraes <ike.moraes93@gmail.com>
2020-05-17 20:56:33 -0300

    melhorias prod

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,10 +1,11 @@
 const merge = require('webpack-merge')
+const webpackBaseConfig = require('./webpack.common.config')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const webpackBaseConfig = require('./webpack.common.config')
 
 module.exports = merge(webpackBaseConfig, {
   optimization: {
+    minimize: true,
     minimizer: [
       new UglifyJsPlugin(),
       new OptimizeCSSAssetsPlugin()
